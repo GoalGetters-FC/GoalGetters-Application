468eeb9716a704402c893d446d8c2db35ae68de5
mattpieterse <63480247+mattpieterse@users.noreply.github.com>
2025-07-02 15:17:02 +0200

    feat(data): add instance method to room database class

--- app/src/main/java/com/ggetters/app/data/local/AppDatabase.kt
+++ app/src/main/java/com/ggetters/app/data/local/AppDatabase.kt
@@ -1,17 +1,21 @@
 package com.ggetters.app.data.local
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverter
 import androidx.room.TypeConverters
-import com.ggetters.app.data.local.dao.UserDao
 import com.ggetters.app.data.local.dao.TeamDao
-import com.ggetters.app.data.local.entities.UserEntity
+import com.ggetters.app.data.local.dao.UserDao
 import com.ggetters.app.data.local.entities.TeamEntity
+import com.ggetters.app.data.local.entities.UserEntity
 import java.util.Date
 
 object DateTypeConverter {
-    @TypeConverter fun fromDate(d: Date?): Long? = d?.time
+    @TypeConverter
+    fun fromDate(d: Date?): Long? = d?.time
+
     @TypeConverter
     fun toDate(t: Long?): Date? = t?.let { Date(it) }
 }
@@ -25,12 +29,32 @@ object DateTypeConverter {
  */
 
 @Database(
-    entities = [UserEntity::class, TeamEntity::class],
-    version = 1,
-    exportSchema = true
+    entities = [UserEntity::class, TeamEntity::class], version = 1, exportSchema = true
 )
 @TypeConverters(DateTypeConverter::class) // if needed for Date <-> Long
 abstract class AppDatabase : RoomDatabase() {
+    companion object {
+        private var INSTANCE: AppDatabase? = null
+        private const val DATABASE_NAME = "expense_database.db"
+
+        /**
+         * Returns a singleton instance of the [AppDatabase].
+         *
+         * @param context The application context.
+         * @return Singleton [AppDatabase] instance.
+         */
+        fun getDatabase(context: Context): AppDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext, AppDatabase::class.java, DATABASE_NAME
+                ).build()
+
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+
     abstract fun userDao(): UserDao
     abstract fun teamDao(): TeamDao
 }
