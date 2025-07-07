9d0c5be4d395502d7a1e19ed1b56622f74693c0c
mattpieterse <63480247+mattpieterse@users.noreply.github.com>
2025-07-07 15:26:31 +0200

    feat(auth): add email credential function to auth validator

--- app/src/main/java/com/ggetters/app/core/utils/AuthValidator.kt
+++ app/src/main/java/com/ggetters/app/core/utils/AuthValidator.kt
@@ -1,6 +1,18 @@
 package com.ggetters.app.core.utils
 
+import android.util.Patterns
+
 /**
  * Helper to validate credentials against Firebase Authentication rulesets.
  */
-object AuthValidator {}
+object AuthValidator {
+    
+    /**
+     * Validates an email address using [Patterns.EMAIL_ADDRESS].
+     *
+     * @return [Boolean] indicating its validity.
+     */
+    fun isValidEAddress(input: String): Boolean {
+        return Patterns.EMAIL_ADDRESS.matcher(input).matches()
+    }
+}
