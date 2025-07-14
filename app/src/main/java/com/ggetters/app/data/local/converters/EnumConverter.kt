d409efeb14495510ed12b5811f47d11a1fb75d23
mattpieterse <63480247+mattpieterse@users.noreply.github.com>
2025-07-15 00:03:46 +0200

    feat(data): create type converter class for enum values

---
+++ app/src/main/java/com/ggetters/app/data/local/converters/EnumConverter.kt
@@ -0,0 +1,12 @@
+package com.ggetters.app.data.local.converters
+
+import androidx.room.RoomDatabase
+import androidx.room.TypeConverter
+
+/**
+ * Type converter for Enum values.
+ *
+ * @see TypeConverter
+ * @see RoomDatabase
+ */
+class EnumConverter {}
