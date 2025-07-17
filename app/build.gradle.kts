25719461a30a3ba3f08106fea29604272631ba5e
mattpieterse <63480247+mattpieterse@users.noreply.github.com>
2025-07-17 17:20:45 +0200

    build(gradle/deps): add dependency for new android splash creen api

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -71,6 +71,8 @@ dependencies {
     implementation("androidx.navigation:navigation-compose:2.7.7")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+    implementation(libs.androidx.core.splashscreen)
+    
 
     // Firebase
 
