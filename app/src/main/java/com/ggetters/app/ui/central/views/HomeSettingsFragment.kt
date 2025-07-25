963e6030e050931dbf9ac61c5d45621770b676b2
musa <ST10293362@vcconnect.edu.za>
2025-07-25 12:26:25 +0200

    cleanup

---
+++ app/src/main/java/com/ggetters/app/ui/central/views/HomeSettingsFragment.kt
@@ -0,0 +1,16 @@
+package com.ggetters.app.ui.central.views
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.ggetters.app.R
+
+class HomeSettingsFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_settings, container, false)
+    }
+}
