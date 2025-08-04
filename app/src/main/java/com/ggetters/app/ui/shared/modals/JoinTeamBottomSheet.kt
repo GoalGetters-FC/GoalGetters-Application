1002fd17c1bbc2cc3251d2ca6a08747014bdecd0
mattpieterse <63480247+mattpieterse@users.noreply.github.com>
2025-08-04 20:45:36 +0200

    feat(ui): create join team modal bottom sheet

---
+++ app/src/main/java/com/ggetters/app/ui/shared/modals/JoinTeamBottomSheet.kt
@@ -0,0 +1,80 @@
+package com.ggetters.app.ui.shared.modals
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.ggetters.app.core.utils.Clogger
+import com.ggetters.app.databinding.ModalBottomSheetJoinTeamBinding
+import com.ggetters.app.ui.shared.models.Clickable
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+class JoinTeamBottomSheet(
+    private val onSubmit: (String, String) -> Unit
+) : BottomSheetDialogFragment(), Clickable {
+    companion object {
+        private const val TAG = "JoinTeamBottomSheet"
+    }
+
+
+    private lateinit var binds: ModalBottomSheetJoinTeamBinding
+
+
+// --- Lifecycle
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? = createBindings(inflater, container)
+
+
+    override fun onViewCreated(
+        view: View, savedInstanceState: Bundle?
+    ) {
+        super.onViewCreated(view, savedInstanceState)
+        setupTouchListeners()
+    }
+
+
+// --- Event Handlers
+
+
+    override fun setupTouchListeners() {
+        binds.btContinue.setOnClickListener(this)
+    }
+
+
+    override fun onClick(view: View?) = when (view?.id) {
+        binds.btContinue.id -> {
+            val teamCode = binds.etCodeTeam.text.toString().trim()
+            val userCode = binds.etCodeUser.text.toString().trim()
+            onSubmit(
+                teamCode, userCode
+            )
+        }
+
+        else -> {
+            Clogger.w(
+                TAG, "Unhandled on-click for: ${view?.id}"
+            )
+        }
+    }
+
+
+// --- UI
+
+
+    /**
+     * Construct the view binding for this fragment.
+     *
+     * @return the root [View] of this fragment within the same context as every
+     *         other invocation of the binding instance. This is crucial because
+     *         otherwise they would exist in different contexts.
+     */
+    private fun createBindings(
+        inflater: LayoutInflater, container: ViewGroup?
+    ): View {
+        binds = ModalBottomSheetJoinTeamBinding.inflate(inflater, container, false)
+        return binds.root
+    }
+}
