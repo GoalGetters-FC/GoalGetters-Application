f4ec195d3865a0c91b832e9a642d13083872d658
mattpieterse <63480247+mattpieterse@users.noreply.github.com>
2025-08-19 21:55:25 +0200

    feat(frag-home-team): create fragment with registered viewmodel

---
+++ app/src/main/java/com/ggetters/app/ui/central/views/HomeTeamFragment.kt
@@ -0,0 +1,11 @@
+package com.ggetters.app.ui.central.views
+
+import androidx.fragment.app.Fragment
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class HomeTeamFragment : Fragment() {
+    companion object {
+        private const val TAG = "HomeTeamFragment"
+    }
+}
