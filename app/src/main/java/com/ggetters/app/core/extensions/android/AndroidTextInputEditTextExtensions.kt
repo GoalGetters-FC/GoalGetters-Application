b02c6146e9ae139b0a9c31c4d7c76b849a0195c5
mattpieterse <63480247+mattpieterse@users.noreply.github.com>
2025-08-24 17:46:00 +0200

    feat: create android text-input-edit-text-extension to attach inline validation logic to the UI

--- app/src/main/java/com/ggetters/app/core/extensions/android/AndroidTextInputEditTextExtensions.kt
+++ app/src/main/java/com/ggetters/app/core/extensions/android/AndroidTextInputEditTextExtensions.kt
@@ -2,11 +2,31 @@ package com.ggetters.app.core.extensions.android
 
 import android.view.ViewParent
 import com.ggetters.app.core.extensions.kotlin.emptyString
+import com.ggetters.app.core.models.results.Final
+import com.ggetters.app.core.validation.ValidationError
 import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
 
 
 // --- Extensions
+
+
+    fun TextInputEditText.addInlineValidation(
+        validator: (String) -> Final<Unit, ValidationError>
+    ) {
+        val textInputLayout = findTextInputLayout()
+        onTextUpdated { text -> 
+            validator(text).apply { 
+                onSuccess { 
+                    textInputLayout?.error = emptyString()
+                }
+                
+                onFailure { error ->
+                    textInputLayout?.error = error.toString()
+                }
+            }
+        }
+    }
 
 
     fun TextInputEditText.setLayoutError(notice: String?) {
