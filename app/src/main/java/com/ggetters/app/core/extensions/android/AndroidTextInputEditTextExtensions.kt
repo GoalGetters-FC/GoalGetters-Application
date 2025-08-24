3460fa6ce8aaf73dfac3919c500ba1cdec1be292
mattpieterse <63480247+mattpieterse@users.noreply.github.com>
2025-08-24 17:30:42 +0200

    feat: create android text-input-edit-text-extension to render an error state and message on its parent layout

---
+++ app/src/main/java/com/ggetters/app/core/extensions/android/AndroidTextInputEditTextExtensions.kt
@@ -0,0 +1,39 @@
+package com.ggetters.app.core.extensions.android
+
+import android.view.ViewParent
+import com.ggetters.app.core.extensions.kotlin.emptyString
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
+
+
+// --- Extensions
+
+
+    fun TextInputEditText.setLayoutError(notice: String?) {
+        val layout = this.findTextInputLayout()
+        layout?.error = notice ?: emptyString()
+    }
+
+
+// --- Internal
+
+
+/**
+ * Find the [TextInputLayout] that contains this [TextInputEditText].
+ * 
+ * **Note:** This extension method will recursively search the parent views of
+ * this [TextInputEditText] until it finds a [TextInputLayout], even when it is
+ * not a direct child of it.
+ */
+private fun TextInputEditText.findTextInputLayout(): TextInputLayout? {
+    var parentView: ViewParent? = this.parent
+    while (parentView != null) {
+        if (parentView is TextInputLayout) {
+            return parentView
+        }
+        
+        parentView = parent.parent
+    }
+
+    return null
+}
