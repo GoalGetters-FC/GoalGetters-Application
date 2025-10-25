48b7d6c8798138bc2c55428bf29864a37bb947db
mattpieterse <63480247+mattpieterse@users.noreply.github.com>
2025-10-25 15:16:55 +0200

    feat: create orphaned function to invoke a webview

---
+++ app/src/main/java/com/ggetters/app/core/extensions/kotlin/BrowserExtensions.kt
@@ -0,0 +1,13 @@
+package com.ggetters.app.core.extensions.kotlin
+
+import android.content.Context
+import androidx.browser.customtabs.CustomTabsIntent
+import androidx.core.net.toUri
+
+fun openBrowserTo(
+    useContext: Context,
+    webViewUrl: String
+) {
+    val intent = CustomTabsIntent.Builder().build()
+    intent.launchUrl(useContext, webViewUrl.toUri())
+}
